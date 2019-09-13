b76325df20251fb044cff9890f68999dd0881202
Michal Terranova <michalterranova@mail.com>
2019-09-13 19:40:50 -0400

    commit finishing product

--- controllers/routes.js
+++ controllers/routes.js
@@ -17,7 +17,7 @@ router.put('/burgers/update', function(req,res){
 });
 
 router.post('/burgers/create', function(req,res){
-    burger.create(req.body.burger_id, function(result){
+    burger.create(req.body.burger_name, function(result){
         console.log(result);
         res.redirect('/');
     });
