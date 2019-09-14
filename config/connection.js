eef2b8bc84dea07977f543068259c955be0d01be
Michal Terranova <michalterranova@mail.com>
2019-09-13 23:58:45 -0400

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,18 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: "burgers_db"
 });
+};
 
 connection.connect (function(err){
     if (err) throw err; 
